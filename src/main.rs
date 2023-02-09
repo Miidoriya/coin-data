2a85937b515e20e13a2248248ea65b1aa584b989
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2023-02-09 20:26:39 +0000

    testing WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
 
+const API_URL: &str = "https://api.coincap.io/v2";
+const START_AND_END: &str = "start=1356931594000&end=1675817253000";
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Cryptocurrency {
     id: String,
@@ -33,23 +36,27 @@ struct CoinInfo {
     current_price: f64,
 }
 
-async fn get_coin_data(name: &str, interval: &str) -> Result<CoinData, reqwest::Error> {
+async fn get_coin_data(url: &str, name: &str, interval: &str) -> Result<CoinData, reqwest::Error> {
     let url = format!(
-        "https://api.coincap.io/v2/assets/{}/history?interval={}&start=1356931594000&end=1675817253000",
-        name, interval
+        "{}/assets/{}/history?interval={}&{}",
+        url, name, interval, START_AND_END
     );
+    println!("url: {}", url);
     let resp = reqwest::get(&url).await?.json::<CoinData>().await?;
+    println!("resp");
     Ok(resp)
 }
 
-async fn get_coins() -> Result<CryptoList, reqwest::Error> {
-    let url = format!("https://api.coincap.io/v2/assets");
+async fn get_coins(url: &str) -> Result<CryptoList, reqwest::Error> {
+    let url = format!("{}/assets", url).to_string();
     let resp = reqwest::get(&url).await?.json::<CryptoList>().await?;
     Ok(resp)
 }
 
-async fn get_coin_info(name: &str, interval: &str) -> Result<CoinInfo, Box<dyn std::error::Error>> {
-    let coin_data = get_coin_data(name, interval).await?;
+async fn get_coin_info(
+    coin_data: CoinData,
+    name: &str,
+) -> Result<CoinInfo, Box<dyn std::error::Error>> {
     let prices = &coin_data.data;
 
     let all_time_high = prices
@@ -105,20 +112,26 @@ fn draw_bar_graph(upper: f64, lower: f64, current: f64, symbol: String) {
 
 #[tokio::main]
 async fn main() {
-    let coin_list = get_coins().await;
+    let coin_list = get_coins(API_URL).await;
     match coin_list {
         Ok(coin_list) => {
             for coin in coin_list.data {
-                let coin_data = get_coin_info(&coin.id, "d1").await;
+                let coin_data = get_coin_data(API_URL, &coin.id, "d1").await;
                 match coin_data {
                     Ok(data) => {
-                        let (upper, lower, current, name) = (
-                            data.all_time_high,
-                            data.all_time_low,
-                            data.current_price,
-                            data.name,
-                        );
-                        draw_bar_graph(upper, lower, current, name);
+                        let coin_info = get_coin_info(data, &coin.id).await;
+                        match coin_info {
+                            Ok(data) => {
+                                let (upper, lower, current, name) = (
+                                    data.all_time_high,
+                                    data.all_time_low,
+                                    data.current_price,
+                                    data.name,
+                                );
+                                draw_bar_graph(upper, lower, current, name);
+                            }
+                            Err(e) => println!("Error: {}", e),
+                        }
                     }
                     Err(e) => println!("Error: {}", e),
                 }
@@ -128,43 +141,115 @@ async fn main() {
     }
 }
 
-/// write some tests for the functions in this module
-/// make the tests detailed and thorough
-/// covering each possible branch of execution
-/// and each possible error condition
-/// use any known crates you like
-/// use any testing framework you like
-/// use any test runner you like
-/// use any test coverage tool you like
-/// use any test coverage reporting tool you like
-/// use any test coverage reporting service you like
-
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[tokio::test]
-    async fn test_get_coin_data() {
-        let coin_data = get_coin_data("bitcoin", "d1").await;
-        assert!(coin_data.is_ok());
-    }
+    use httpmock::Method::GET;
+    use httpmock::MockServer;
 
     #[tokio::test]
     async fn test_get_coins() {
-        let coins = get_coins().await;
-        assert!(coins.is_ok());
+        let mock_server = MockServer::start();
+        let mock = mock_server.mock(|when, then| {
+            when.method(GET).path("/v2/assets");
+            then.status(200)
+                .header("content-type", "application/json")
+                .body(
+                    r#"{
+                "data": [
+                    {
+                        "id": "bitcoin",
+                        "rank": "1",
+                        "symbol": "BTC",
+                        "name": "Bitcoin"
+                    },
+                    {
+                        "id": "ethereum",
+                        "rank": "2",
+                        "symbol": "ETH",
+                        "name": "Ethereum"
+                    }
+                ]
+            }"#,
+                );
+        });
+        let coin_list = get_coins(&mock_server.url("/v2")).await;
+        assert!(coin_list.is_ok());
+        let coin_list = coin_list.unwrap();
+        assert_eq!(coin_list.data.len(), 2);
+        assert_eq!(coin_list.data[0].id, "bitcoin");
+        assert_eq!(coin_list.data[0].rank, "1");
+        assert_eq!(coin_list.data[0].symbol, "BTC");
+        assert_eq!(coin_list.data[0].name, "Bitcoin");
+        assert_eq!(coin_list.data[1].id, "ethereum");
+        assert_eq!(coin_list.data[1].rank, "2");
+        assert_eq!(coin_list.data[1].symbol, "ETH");
+        assert_eq!(coin_list.data[1].name, "Ethereum");
+        mock.assert();
     }
 
     #[tokio::test]
+    async fn test_get_coin_data() {
+        let url_path = format!("/v2/assets/bitcoin/history?interval=d1&{}", START_AND_END);
+        let mock_server = MockServer::start();
+        let mock = mock_server.mock(|when, then| {
+            when.method(GET).path(&url_path);
+            then.status(200)
+                .header("content-type", "application/json")
+                .body(
+                    r#"{
+                "data": [
+                    {
+                        "priceUsd": "13.8"
+                        "time": 1356998400000,
+                    },
+                    {
+                        "priceUsd": "13.98"
+                        "time": 1357084800000,
+                    }
+                ]
+            }"#,
+                );
+        });
+        println!("{}", mock_server.url(&url_path));
+        println!("{}", mock_server.url("/v2"));
+        let coin_data = get_coin_data(&mock_server.url("/v2"), "bitcoin", "d1").await;
+        assert!(coin_data.is_ok());
+        let coin_data = coin_data.unwrap();
+        assert_eq!(coin_data.data.len(), 2);
+        assert_eq!(coin_data.data[0].time, 1356998400000);
+        assert_eq!(coin_data.data[0].priceUsd, "13.8");
+        assert_eq!(coin_data.data[1].time, 1357084800000);
+        assert_eq!(coin_data.data[1].priceUsd, "13.98");
+        mock.assert();
+    }
+
+    #[tokio::test]
+    // write a test for the function get_coin_info which doesn't use a mock server as the function doesn't need to make any HTTP requests
     async fn test_get_coin_info() {
-        let coin_info = get_coin_info("bitcoin", "d1").await;
+        // mock a CoinData struct
+        let coin_data = CoinData {
+            data: vec![
+                PriceData {
+                    time: 1356998400000,
+                    priceUsd: "13.8".to_string(),
+                },
+                PriceData {
+                    time: 1357084800000,
+                    priceUsd: "13.98".to_string(),
+                },
+                PriceData {
+                    time: 1357084800000,
+                    priceUsd: "13.9".to_string(),
+                },
+            ],
+        };
+        let coin_info = get_coin_info(coin_data, "bitcoin").await;
         assert!(coin_info.is_ok());
-    }
-
-    #[tokio::test]
-    async fn test_draw_bar_graph() {
-        draw_bar_graph(100.0, 0.0, 50.0, "BTC".to_string());
+        let coin_info = coin_info.unwrap();
+        assert_eq!(coin_info.name, "bitcoin");
+        assert_eq!(coin_info.all_time_high, 13.98);
+        assert_eq!(coin_info.all_time_low, 13.8);
+        assert_eq!(coin_info.current_price, 13.9);
     }
 }
